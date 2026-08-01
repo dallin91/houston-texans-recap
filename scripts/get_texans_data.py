d776da9bc73b61c00917d22ae44f572a11b1e0f2
Dallin Reeves <dallinreeves@gmail.com>
2026-07-31 23:13:32 -0500

    initial commit

---
+++ scripts/get_texans_data.py
@@ -0,0 +1,17 @@
+"""
+The purpose of this script is to pull all game and player data for the Houston Texans from 2002-2025.
+This data will be exported to .csv files in /data/raw
+"""
+
+import nflreadpy as nfl
+import polars as pl
+
+# Pull and write Houston Texans game data to CSV
+team_stats = nfl.load_team_stats(seasons=True)
+hou_stats = team_stats.filter(pl.col("team") == "HOU")
+hou_stats.write_csv("./data/raw/hou_stats.csv")
+
+# Pull and write Texans player stats to CSV
+player_stats = nfl.load_player_stats([2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025])
+hou_player_stats = player_stats.filter(pl.col("team") == "HOU")
+hou_player_stats.write_csv("./data/raw/hou_player_stats.csv")
---
+++ scripts/load_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PORT = os.getenv('DB_PORT')
+DB_HOST = os.getenv('DB_HOST')
+
+engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+
+# Prep player and team data to be exported to Postgres
+player_df = pd.read_csv("./data/raw/hou_player_stats.csv")
+player_df.columns = player_df.columns.str.strip().str.lower().str.replace(' ', '_')
+
+team_df = pd.read_csv("./data/raw/hou_stats.csv")
+team_df.columns = team_df.columns.str.strip().str.lower().str.replace(' ', '_')
+
+# Load data to database
+player_df.to_sql('player_stats', engine, if_exists='replace', index=False)
+team_df.to_sql('team_stats', engine, if_exists='replace', index=False)
