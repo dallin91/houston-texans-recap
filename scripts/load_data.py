75d51cf2e818efa33543c007df60e0de1a2f6c51
Dallin Reeves <dallinreeves@gmail.com>
2026-08-11 16:18:11 -0500

    updated script to load new csvs to DB

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -17,13 +17,17 @@ engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT
 player_df = pd.read_csv("./data/raw/hou_player_stats.csv")
 player_df.columns = player_df.columns.str.strip().str.lower().str.replace(' ', '_')
 
-team_df = pd.read_csv("./data/raw/hou_stats.csv")
+team_df = pd.read_csv("./data/raw/team_stats.csv")
 team_df.columns = team_df.columns.str.strip().str.lower().str.replace(' ', '_')
 
 schedule_df = pd.read_csv("./data/raw/hou_schedules.csv")
 schedule_df.columns = schedule_df.columns.str.strip().str.lower().str.replace(' ', '_')
 
+pbp_df = pd.read_csv("./data/raw/hou_pbp.csv")
+pbp_df.columns = pbp_df.columns.str.strip().str.lower().str.replace(' ', '_')
+
 # Load data to database
 player_df.to_sql('player_stats', engine, if_exists='replace', index=False)
 team_df.to_sql('team_stats', engine, if_exists='replace', index=False)
 schedule_df.to_sql('schedule_stats', engine, if_exists='replace', index=False)
+pbp_df.to_sql('pbp_stats', engine, if_exists='replace', index=False)
